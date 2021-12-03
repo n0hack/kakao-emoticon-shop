15583368fc830b0b783550233c28a878be00a0ff
n0hack <nohack-@naver.com>
2021-12-03 20:27:56 +0900

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './assets/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          lodaer: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+      { test: /\.css$/, use: 'css-loader' },
+    ],
+  },
+};
