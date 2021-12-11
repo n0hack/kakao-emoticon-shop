00a5c1b80c208867cda77f97bd8165f8d82c617a
n0hack <nohack-@naver.com>
2021-12-11 20:30:45 +0900

    [fix] 드래그 슬라이드 보정

--- assets/js/sidepage.mjs
+++ assets/js/sidepage.mjs
@@ -1,7 +1,7 @@
 const elMenuList = document.querySelector('.mypage__menus');
 const elMenuItems = document.querySelectorAll('.mypage__menu');
 const menuListClientWidth = elMenuList.clientWidth;
-const menuListScrollWidth = elMenuList.clientWidth;
+const menuListScrollWidth = elMenuList.scrollWidth;
 const menuItemsTotalWidth = [...elMenuItems].reduce((acc, item) => acc + item.clientWidth, 0);
 const lastMenuPaddingRight = parseInt(getComputedStyle(elMenuItems[elMenuItems.length - 1]).paddingRight);
 const lastMenu = elMenuItems[elMenuItems.length - 1];
@@ -14,6 +14,7 @@ console.log(menuItemsTotalWidth);
 console.log(lastMenuPaddingRight);
 console.log(lastMenu.getBoundingClientRect());
 console.log(parseInt(getComputedStyle(elMenuList).transform.split(/[^\-0-9]+/g)[5]));
+console.log(menuListScrollWidth - menuListClientWidth);
 
 class DragSlide {
   constructor() {}
@@ -64,13 +65,20 @@ const correctTranslate = () => {
       translateX = 0;
     }
 
-    // 마지막 getBoundingClientRect
     if (translateX < -(menuListScrollWidth - menuListClientWidth)) {
       elMenuList.style.transform = `translateX(${-(menuListScrollWidth - menuListClientWidth)}px)`;
       elMenuList.style.transition = 'all 0.3s ease-in-out';
       translateX = -(menuListScrollWidth - menuListClientWidth);
     }
+    console.log(translateX);
   }
 };
 
 elMenuList.addEventListener('mousedown', handleMouseDown);
+window.addEventListener('resize', () => {
+  if (translateX <= -(menuListScrollWidth - menuListClientWidth)) {
+    console.log('dd');
+  }
+  // TODO: Resize 이벤트 (스크롤 사이즈가 클라이언트 사이즈랑 같으면 기본)
+  // TODO: Resize 이벤트 스크롤 사이즈가 더 크고, 맨 뒤인 경우 보정 처리
+});
